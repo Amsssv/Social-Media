970b9c07f135f7b94ad7da91ce2fb6b180e5e4d4
Amsssv <Amsssv@yandex.ru>
2022-07-20 23:06:41 +0300

    conect jest

---
+++ server/tests/test-auth.ts
@@ -0,0 +1,7 @@
+import express, {Express, Response, Request} from 'express';
+
+const app: Express | Response = express();
+
+describe('User can successfully login', () => {
+    expect(res.status).toBe(200);
+})
