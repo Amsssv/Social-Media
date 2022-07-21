22be6dcd4ddd32fcb7b806c32e0caff04222a205
Amsssv <Amsssv@yandex.ru>
2022-07-21 22:31:49 +0300

    added 2 events in test

--- server/__tests__/auth-test.ts
+++ server/__tests__/auth-test.ts
@@ -1,12 +1,36 @@
-import request from "supertest";
+import supertest from "supertest";
 import app from '../index';
 
 describe("Authentication and Authorization form", () => {
+
     test("User can successfully login", async () => {
-        const response = await request(app).post("/login");
-        expect(response.statusCode).toBe(200);
-        expect(typeof response.body.login === 'string').toBe(true);
-        expect(typeof response.body.password === 'string').toBe(true);
-    });
+        const res = await supertest(app)
+            .post("/login")
+            .send({
+                login: 'user',
+                password: 'user',
+            });
+
+        expect(res.statusCode).toBe(200);
+        expect(typeof res.body.accessToken === "string").toBe(true)
+        expect(typeof res.body.refreshToken === "string").toBe(true)
+
+        const {statusCode, body, header} = await supertest(app)
+            .get("/refresh")
+            .send({
+                refreshToken: res.body.refreshToken,
+            });
+
+        expect(statusCode).toBe(200);
+        expect(typeof body.accesstoken === 'string').toBe(true);
+        expect(typeof  header.cookie.value === 'string').toBe(true);
+    })
+
+    test("User gets 403 on invalid credentials", async () => {
+        const res = await supertest(app)
+            .post("/login");
+
+        expect(res.statusCode).toBe(403);
+    })
 });
 
