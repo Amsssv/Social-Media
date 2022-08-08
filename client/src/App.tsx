38f818e081bd396ca25727a121f87e85f3302dc1
Amsssv <Amsssv@yandex.ru>
2022-08-08 15:13:30 +0300

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
       </header>
         <body>
-        <h1> Hello world</h1>
+        <h1 className="text-3xl font-bold underline"> Hello world</h1>
         </body>
     </div>
   );
---
+++ client/webpack.config.js
@@ -0,0 +1,50 @@
+import {dirname, resolve} from 'path';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-typescript", "@babel/preset-react"],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [new HtmlWebpackPlugin({template: './public/index.html'})],
+};
